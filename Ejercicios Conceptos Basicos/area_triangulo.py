9cf4f5bb3190f8deebc67a595b1a994bb0177cbf
Daniel Velez <daniel.cuaical@correounivalle.edu.co>
2019-12-03 15:05:12 -0500

    Encontrar el area un triangulo dada su base y altura

---
+++ Ejercicios Conceptos Basicos/area_triangulo.py
@@ -0,0 +1,7 @@
+#Dado los valores ingresados por el usuario (base, altura) calcular y mostrar en pantalla el área de un triángulo.
+
+base = int(input("Ingrese la base del triángulo:"));
+altura = int(input("Ingrese la altura del triangulo:"));
+area = (base*altura)/2;
+
+print("El area del triangulo es:", area);
