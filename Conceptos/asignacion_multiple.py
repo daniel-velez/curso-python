48e2482f3f713834a1dec315de8da25c647a8c9d
Daniel Velez <daniel.cuaical@correounivalle.edu.co>
2019-12-03 13:26:31 -0500

    first commit

---
+++ Conceptos/asignacion_multiple.py
@@ -0,0 +1,4 @@
+valor_uno, valor_dos, valor_tres = 1, "hola", None;
+print(valor_uno);
+print(valor_tres);
+print(valor_dos);
---
+++ Conceptos/operadores.py
@@ -0,0 +1,18 @@
+variable_uno = 10;
+variable_dos = 18;
+
+#Operadores lógicos
+
+mayor = variable_uno > variable_dos;
+menor = variable_uno < variable_dos;
+mayor_igual = variable_uno >= variable_dos;
+menor_igual = variable_uno <= variable_dos;
+igual = variable_uno == variable_dos; #también puede ser así variable_uno is varialbe_dos;
+diferente = variable_uno != variable_dos;
+
+print(mayor);
+print(menor);
+print(mayor_igual);
+print(menor_igual);
+print(igual);
+print(diferente);
