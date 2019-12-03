84c7e4297a1d985e0a5b3bbf333d2f8a3d5527d5
Daniel Velez <daniel.cuaical@correounivalle.edu.co>
2019-12-03 14:34:14 -0500

    Cómo realizar diferentes tipos de comentarios

---
+++ Conceptos/comentarios.py
@@ -0,0 +1,14 @@
+#Soy un comentario de una sola linea jsjsjs ke creisi
+
+"""
+Este es un 
+comentario 
+con saltos 
+de linea
+"""
+
+'''
+Esto es lo mismo
+de arriba pero
+con 3 apóstrofes
+'''
--- Conceptos/variables.py
+++ Conceptos/variables.py
@@ -2,3 +2,8 @@ CONSTANTE = "soy una constante";
 tutor = "Codi";
 print(tutor);
 print(CONSTANTE);
+#tipado dinámico
+dato = "hola";
+dato = 10;
+dato = True;#El último dato guardado será el que se muestre
+print(dato);
