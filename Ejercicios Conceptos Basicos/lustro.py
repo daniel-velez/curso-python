b110462ca20919b62b4158887030ccd1007e3188
Daniel Velez <daniel.cuaical@correounivalle.edu.co>
2019-12-03 22:31:42 -0500

    Muestra cuantos segundos tiene un lustro

---
+++ Ejercicios Conceptos Basicos/lustro.py
@@ -0,0 +1,11 @@
+#Mostrar en pantalla la cantidad de segundos que tiene un lustro
+
+lustro = 5;
+año = 365;
+dia = 24;
+hora = 60;  
+minutos = 60;
+
+resultado = lustro*año*dia*hora*minutos;
+
+print("Un lustro tiene", resultado, "segundos");
