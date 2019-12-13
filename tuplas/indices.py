03fb6ec9d9c2637ba6b232153e678dce6a7743bb
Daniel Velez <daniel.cuaical@correounivalle.edu.co>
2019-12-12 22:04:37 -0500

    tuplas

---
+++ tuplas/indices.py
@@ -0,0 +1,12 @@
+#Las tuplas son inmutables, después de ser creadas no se pueden modificar.
+
+tupla = (1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+
+elemento = tupla[-1];#Retorna el último elemento.
+#print(elemento);
+
+#Las tuplas funcionan de manera similar a las listas.
+
+sub = tupla[:9:2];#SUb tupla con los primeros 9 elementos y saltos dobles.
+#print(sub);
+
