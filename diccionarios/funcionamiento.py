d1627fbf7965e9c99aa6a9c95e79afc3908712da
Daniel Velez <daniel.cuaical@correounivalle.edu.co>
2019-12-14 16:25:21 -0500

    correcion de errores

---
+++ diccionarios/funcionamiento.py
@@ -0,0 +1,8 @@
+diccionario = {}; #Se crea una variable de tipo diccionario
+
+diccionario["nombre"] = "Codi"; #Agregar una llave con su valor
+
+valor = diccionario["nombre"]; #Obtener el valor de una llave
+
+diccionario["nombre"] = 90;
+print(diccionario);
