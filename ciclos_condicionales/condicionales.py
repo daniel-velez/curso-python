a4f81e0083ba39dbc20976087c89fc159468d6d6
Daniel Velez <daniel.cuaical@correounivalle.edu.co>
2019-12-17 10:57:20 -0500

    Uso del condicional if

---
+++ ciclos_condicionales/condicionales.py
@@ -0,0 +1,19 @@
+color_luz = "red";
+
+if color_luz == "verde":
+    print("Puede continuar");
+
+elif color_luz == "amarillo":
+    print("alto parcial");
+
+elif color_luz == "rojo":
+    print('Alto');
+
+#Valores falsos: False, "", '', [], (), {}, 0, 0.0
+variable = True;
+
+if variable:
+    print("Es verdadero");
+
+else:
+    print("Es falso");
