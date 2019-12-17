acd3c4d21c6e51f5c872043cbabd119a869d3b4d
Daniel Velez <daniel.cuaical@correounivalle.edu.co>
2019-12-17 10:39:14 -0500

    uso del None

---
+++ ciclos_condicionales/none.py
@@ -0,0 +1,6 @@
+variable = [1, 2, 3, 4];
+variable = None #Para definir un valor vacio
+
+print(variable);
+
+#None es considerado un False al igual que False, 0 , 0.0, "" o '', [], (), {}. Todos los demas valores serán considerados verdaderos
