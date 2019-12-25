9e57d3ba8386f366e950c684835cfed1088cda92
Daniel <daniel.cuaical@correounivalle.edu.co>
2019-12-25 11:41:48 -0500

    Trabajar con funciones anidadas

---
+++ funciones/anidadas.py
@@ -0,0 +1,14 @@
+def comenzar_play_list(lista):
+
+    def reproducir():
+        nonlocal lista; #para modificar la variable local
+        lista = [1, 2, 3];
+        for val in lista:
+            print(val);
+    
+    reproducir();
+    print(lista);
+
+lista = ["track 1", "track 2", "track 3", "track 4"];
+
+comenzar_play_list(lista);
