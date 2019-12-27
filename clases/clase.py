50f717a1b0fa0a6f54162f1389a10c46c06ec6f7
Daniel <daniel.cuaical@correounivalle.edu.co>
2019-12-26 22:03:46 -0500

    Atributos y metodos de una clase

--- clases/clase.py
+++ clases/clase.py
@@ -1,9 +1,23 @@
 class Usuario:
+
+    def __init__(self, username='', correo='', nombre=''):
+        self.username = username;
+        self.correo = correo;
+        self.nombre = nombre;
+
     def Saludo(self):
-        print("Hola, soy un usuario");
+        return "Hola, soy un usuario " + self.nombre;
+    
+    def mostrar_username(self):
+        print(self.username);
+    
+    def mostrar_correo(self):
+        print(self.correo);
 
-pro = Usuario();
-bando = Usuario();
+pro = Usuario('ejemplo', 'pro@gmail.com', 'pro');
+bando = Usuario(correo='bando@yopmail.com');
 
-pro.Saludo();
-bando.Saludo();
+resultado = pro.Saludo();
+print(resultado);
+
+bando.mostrar_correo();
