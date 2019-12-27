cbe620dfe2a1b058551901cdd7fa3f49772ab03b
Daniel <daniel.cuaical@correounivalle.edu.co>
2019-12-27 13:34:11 -0500

    Ejemplo de herencia multiple

--- clases/herencia.py
+++ clases/herencia.py
@@ -5,16 +5,32 @@ class Animal:
     def dormir(self):
         print('Durmiendo...');
 
-class Perro(Animal): #Animal se vuelve la clase padre
+class Mascota:
+    def fecha_adopcion(self, fecha):
+        self.fecha_de_adopcion = fecha;
+
+class Perro(Animal, Mascota): #Animal se vuelve la clase padre
     def __init__(self, nombre):
         self.nombre = nombre;
     
     def ladrar(self):
         print('Ladrando...');
 
-firulais = Perro('firulais');
+class Gato(Animal, Mascota):
+    def ronronear(self):
+        print('Ronroneando...');
 
-firulais.comer();
+firulais = Perro('firulais');
+firulais.fecha_adopcion('12/11/2018');
+print(firulais.fecha_de_adopcion);
 firulais.ladrar();
 firulais.dormir();
+
+print('\n');
+
+bola_de_nieve = Gato();
+bola_de_nieve.dormir();
+
+#En caso de que haya un método en común primero se ejecutara el de la clase principal, si no está ese método pasa a buscar a las clases padre.
+
         
